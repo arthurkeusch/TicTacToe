cb7ad286da0faa66617f9b241a514ee59af1ba0a
Lysdoryah <114756789+Lysdoryah@users.noreply.github.com>
2024-03-29 09:35:49 +0100

    Update settings.cpp

--- V5/settings.cpp
+++ V5/settings.cpp
@@ -1,14 +1,29 @@
 #include "settings.h"
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 SettingsWidget::SettingsWidget(QWidget *parent) : QWidget(parent)
 {
+    QVBoxLayout *vbox = new QVBoxLayout();
+    QHBoxLayout *hbox = new QHBoxLayout();
+
     comboBoxResolution = new QComboBox(this);
     comboBoxResolution->addItem("640x480");
     comboBoxResolution->addItem("800x600");
     comboBoxResolution->addItem("1024x768");
 
     buttonApply = new QPushButton("Apply", this);
+    QPushButton *quit = new QPushButton("Quit", this);
 
+    hbox->addWidget(comboBoxResolution, 0);
+    hbox->addWidget(buttonApply, 1);
+    vbox->addLayout(hbox, 0);
+    vbox->addWidget(quit, 2, Qt::AlignTop);
+
+    setLayout(vbox);
+
+    connect(quit, &QPushButton::clicked, qApp, &QApplication::quit);
     connect(buttonApply, &QPushButton::clicked, this, &SettingsWidget::applySettings);
 }
 
